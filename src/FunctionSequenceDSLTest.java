4f00e141fa98388d6500b44797fb8758f512dbee
Serge Stinckwich <Serge.Stinckwich@gmail.com>
2012-03-07 13:30:37 +0700

    Last version

--- src/FunctionSequenceDSLTest.java
+++ src/FunctionSequenceDSLTest.java
@@ -19,4 +19,35 @@ public class FunctionSequenceDSLTest {
 public final void testTruth() {
 	Assert.assertEquals(true, true);
 	}
+
+@Test
+public final void testComputerWithoutDevices(){
+  Computer c = new Computer(null, null);
+  Assert.assertEquals(c.getProcessor(), null);
+}
+
+@Test
+public final void testComputerStatement(){
+  Assert.assertEquals(ComputerBuilder.script1().getProcessor(), null);
+}
+@Test
+public final void testComputerWith1Processor(){
+  Assert.assertEquals(ComputerBuilder.script2().getProcessor().getCores(), 2);
+  Assert.assertEquals(ComputerBuilder.script2().getProcessor().getType(), Processor.Type.i386);
+}
+@Test
+public final void testComputerWith1Processor1Disk(){
+  Assert.assertEquals(ComputerBuilder.script3().getProcessor().getCores(), 2);
+  Assert.assertEquals(ComputerBuilder.script3().getProcessor().getType(), Processor.Type.i386);
+  Assert.assertEquals(ComputerBuilder.script3().getDisks().length, 1);
+  Assert.assertEquals(ComputerBuilder.script3().getDisks()[0].getSize(), 150);
+}
+
+@Test
+public final void testComputerWith1Processor2Disk(){
+  Assert.assertEquals(ComputerBuilder.script4().getProcessor().getCores(), 2);
+  Assert.assertEquals(ComputerBuilder.script4().getProcessor().getType(), Processor.Type.i386);
+  Assert.assertEquals(ComputerBuilder.script4().getDisks().length, 2);
+  Assert.assertEquals(ComputerBuilder.script4().getDisks()[0].getSize(), 150);
+}
 }
